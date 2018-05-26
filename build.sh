209515973e26986695c3572184b8269b30380f09
masukomi <masukomi@masukomi.org>
2018-05-26 17:33:11 -0400

    simplified build.sh - git stash stuff borked

--- build.sh
+++ build.sh
@@ -3,17 +3,11 @@ STASHED=0
 VERSION="dev_version"
 if [ "$1" != "" ]; then 
   VERSION=$1
-  git stash save "snapshot: $(date)" 
-  STASHED=$?
-  if [ "$STASHED" == "0" ]; then 
-    git stash apply "stash@{0}" > /dev/null 2>&1
-  fi
   perl -pi -e "s/VERSION_NUMBER_HERE/$1/" src/oho.cr
 fi
 crystal build --release src/oho.cr
-git checkout HEAD src/oho.cr
-if [ $STASHED -eq 0 ]; then 
-  git stash pop > /dev/null 2>&1
+if [ "$1" != "" ]; then 
+  perl -pi -e "s/$1/VERSION_NUMBER_HERE/" src/oho.cr
 fi
 
 version_dir="oho_$VERSION"

7c5e41cc779b730aee14bacbd602e5137a11a6c6
masukomi <masukomi@masukomi.org>
2018-06-17 10:34:36 -0400

    build.sh has more verbose output and generates sha for use in homebrew

--- build.sh
+++ build.sh
@@ -5,11 +5,12 @@ if [ "$1" != "" ]; then
   VERSION=$1
   perl -pi -e "s/VERSION_NUMBER_HERE/$1/" src/oho.cr
 fi
+echo "compiling..."
 crystal build --release src/oho.cr
 if [ "$1" != "" ]; then 
   perl -pi -e "s/$1/VERSION_NUMBER_HERE/" src/oho.cr
 fi
-
+echo "creating compressed release file..."
 version_dir="oho_$VERSION"
 rm -rf $version_dir
 mkdir $version_dir
@@ -17,7 +18,10 @@ cp oho $version_dir/
 tar -czf $version_dir.tgz $version_dir
 rm -rf $version_dir
 
+echo "here's your SHA for homebrew"
+shasum -a 256 $version_dir.tgz
 
+echo "examining binary for dylib requirements..."
 # copy all the optional ones
 # rm -rf dylibs/*
 DYLIBS=`otool -L oho | grep "/opt" | awk -F' ' '{ print $1 }'`
@@ -29,3 +33,4 @@ do
   # install_name_tool -change $dylib "~/lib/$(basename $dylib)" oho
 done
 
+echo "Done."
